ef6ed8c33a461edff32f055512ffa905d5d2003c
eileen cano <eileencano45@gmail.com>
2023-12-02 11:52:05 -0500

    actualizacion portafolio

--- Index.php
+++ Index.php
@@ -45,22 +45,132 @@
    <div class= "col-md-2"></div>
 
    <div class= "col-md-4">
-    <img src="assets/profile-pic.png" alt=""></div>
-    
+    <img src="portafolio.png" class="img-" alt="..."></div>
 
-   <div class= "col-md-4">
-   
-   
+   <div class= "col-md-4 text-center"><br><br><br><br><br>
+   <div class="container-body">
+        <h6 class="text-md-center fw-bold">Hello!,I'm</h6>
+        <h1 class="text-md-center fw-bold">Eileen Cano</h1>
+        <h3 class="text-md-center fw-bold">frontend Developer</h3></div>
+        
+        <button type="button" class="btn btn-light">Download CV</button>
+        <button type="button" class="btn btn-dark">Contact info</button>
+       
+
+        <div class="container  p-3">
+        <img src="assets/github.png" class=img-fluid width=30px  alt="...">
+        <img src="assets/linkedin.png" class=img-fluid  width=30px alt="..."> 
+        </div>
+
    </div>
 
 
    <div class= "col-md-2"></div>
 
  </div>
+ 
+ 
+ <br><br><br><br><br>
+
+
+<!--segunda parte-->
+ <!--About-->
+ 
+
+  <div class="row">
+
+   <div class="container-body">
+    <h6 class="text-md-center fw-bold">Get To Know More</h6>
+    <h1 class="text-md-center fw-bold">About Me</h1>
+    </div><br><br><br>
+
+  <div class= "col-md-2"></div>
+
+  <div class= "col-md-4"><br><br><br>
+   <img src="porta2.jpeg" class="img"   alt="...">
+  </div>
+  
+
+  <div class="col-md-2 text-center"><br><br><br><br><br><br>
+    <div class="card" style="width: 18rem;">
+    <div class="container p-3">
+    <img src="assets/experience.png" class="img-fluid" width="30PX" alt="logo">
+   </div>
+        <div class="card-body">
+          <h5 class="text-md-center fw-bold">Experiencie</h5>
+          <h6 class="card-title"></h6>
+          <h6 class="card-subtitle mb-2 text-body-secondary">frontend</h6>
+         
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-2 text-center"><br><br><br><br><br><br>
+    <div class="card" style="width: 18rem;">
+    <div class="container p-3">
+    <img src="assets/education.png" class="img-fluid" width="30PX" alt="logo">
+   </div>
+        <div class="card-body">
+          <h5 class="text-md-center fw-bold">Education</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Academic Bachelor</h6>
+        </div>
+         
+      </div><br><br><br>
+      
+    </div>
+</div> 
+
+
+<img src="assets/arrow.png" class="rounded float-end" width="30px" alt="logo">
+<br><br><br><br>
+
+<!--Tercera parte-->
+ <!--experience-->
+
+ 
+
+<div class="container-body">
+ <h6 class="text-md-center fw-bold">Explore My</h6>
+ <h1 class="text-md-center fw-bold">Experience</h1>
+ </div><br><br><br>
+ 
+ <div class="row">
+
+ <div class="col-md-2"></div>
+
+  <div class="col-md-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="text-center">Frontend</h5>
+        <img src="assets/checkmark.png" class="img-fluid" width="30PX" alt="logo">css<br><br>
+        <img src="assets/checkmark.png" class="img-fluid" width="30PX" alt="logo">js<br><br>
+        <img src="assets/checkmark.png" class="img-fluid" width="30PX" alt="logo">html<br><br>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="text-center">Fronted development</h5>
+        <img src="assets/checkmark.png" class="img-fluid" width="30PX" alt="logo"><br><br>
+        <img src="assets/checkmark.png" class="img-fluid" width="30PX" alt="logo"><br><br>
+        <img src="assets/checkmark.png" class="img-fluid" width="30PX" alt="logo"><br><br>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<img src="assets/arrow.png" class="rounded float-end" width="30px" alt="logo">
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>  
    
+
   </body>
 </html>
